afdf4a5b6dc9b49f90db7e2d83af68df98ed079c
Dhruvin Jariwala <dhruvinjariwala422002@gmail.com>
2024-06-14 18:26:04 +0530

    validation

--- Models/User.cs
+++ Models/User.cs
@@ -8,16 +8,19 @@ namespace StanfordHospital.Models
 
         [Required]
         [Display(Name = "First Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First Name can only contain letters.")]
         public string? FirstName { get; set; }
         [Display(Name = "Last Name")]
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last Name can only contain letters.")]
         public string? LastName { get; set; }
         [Required]
         [EmailAddress]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please Enter Valid Email Address")]
         public string? Email { get; set; }
         [Display(Name = "Phone No")]
         [Required]
-        [MaxLength(10)]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Phone No must be 10 digits.")]
         public string? PhoneNo { get; set; }
         [Display(Name ="Address")]
         [Required]
